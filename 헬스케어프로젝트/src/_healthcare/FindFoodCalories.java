a42e0317a7f6f393f73451be2c5653c704466f69
gayoung <7rmile@gmail.com>
2024-02-21 14:52:57 +0900

    음식을 검색하면 그 음식 단어를 포함한 음식들이 디비에서 끌고옵니다. 그리고 그 음식을 선택할 수 있어요. 확인 버튼을 누르면 디비에 이제 저장하게 할 건데 그건 DietRecords클래스를 먼저 코드 짜고 해야 할 것 같습니다.

--- 헬스케어프로젝트/src/_healthcare/FindFoodCalories.java
+++ 헬스케어프로젝트/src/_healthcare/FindFoodCalories.java
@@ -2,67 +2,242 @@ package _healthcare;
 
 import javax.swing.JFrame;
 import javax.swing.SpringLayout;
+import javax.swing.table.DefaultTableModel;
+
+import dbutil.MySqlConnectionProvider;
+
 import javax.swing.JTextField;
 import javax.swing.JButton;
-import javax.swing.JList;
-import javax.swing.JCheckBoxMenuItem;
-import javax.swing.JMenuItem;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
+import javax.swing.JScrollPane;
 
-public class FindFoodCalories extends JFrame{
+import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.awt.event.ActionEvent;
+import javax.swing.JTable;
+import javax.swing.JLabel;
+import javax.swing.JTextArea;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+
+public class FindFoodCalories extends JFrame {
 	private JTextField textField;
-	public FindFoodCalories() {
+	private JButton btnNewButton;
+	private JTable resultTable;
+	private DefaultTableModel tableModel;
+	private JButton btnNewButton_1;
+	private String user_id;
+	// 현재 날짜
+	java.util.Date currentDate = new java.util.Date();
+	// sql에 넣기 위해 날짜를 date형식으로 변경
+	Date sqlDate = new Date(currentDate.getTime());
+
+	public FindFoodCalories(String loginId) {
+		this.user_id = loginId;
 		extracted();
-		
-		
 		showGUI();
-		
 	}
-	private void createFoodList() {
-		
+
+	private void findFoodList(String findFoodName) {
+		String selectQuery = "SELECT * FROM foodcalories WHERE foodName LIKE ? LIMIT 0,1000000";
+		List<FoodData> list = new ArrayList<>();
+
+		try (Connection conn = MySqlConnectionProvider.getConnection();
+				PreparedStatement pst = conn.prepareStatement(selectQuery);) {
+			pst.setString(1, "%" + findFoodName + "%");
+
+			ResultSet rs = pst.executeQuery();
+			while (rs.next()) {
+				String foodName = rs.getString("foodName");
+
+				double oneServing = rs.getDouble("oneServing");
+				String unit = rs.getString("unit");
+				String oneServingAndUnit = oneServing + unit;
+
+				double calories = rs.getDouble("calories");
+
+				FoodData foodData = new FoodData(foodName, oneServingAndUnit, calories);
+				list.add(foodData);
+
+			}
+
+			// 검색 결과를 테이블에 업데이트
+			updateTable(list);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
 	}
-	
-	
+
+	private void updateTable(List<FoodData> list) { // JTable업데이트하는 메소드
+		// 테이블 모델 초기화
+		tableModel.setRowCount(0);
+
+		// 검색 결과를 테이블 모델에 추가
+		for (FoodData foodData : list) {
+			tableModel
+					.addRow(new Object[] { foodData.getFoodName(), foodData.getOneServing(), foodData.getCalories() });
+		}
+	}
+
 	private void showGUI() {
-		setSize(500, 500);
+		setSize(484, 592);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setVisible(true);
 	}
-	
-	
+
 	private void extracted() {
 		setTitle("음식칼로리");
 		SpringLayout springLayout = new SpringLayout();
 		getContentPane().setLayout(springLayout);
-		
+
 		textField = new JTextField();
-		springLayout.putConstraint(SpringLayout.SOUTH, textField, -193, SpringLayout.SOUTH, getContentPane());
+		springLayout.putConstraint(SpringLayout.NORTH, textField, 10, SpringLayout.NORTH, getContentPane());
+		springLayout.putConstraint(SpringLayout.WEST, textField, 97, SpringLayout.WEST, getContentPane());
 		getContentPane().add(textField);
 		textField.setColumns(10);
-		
-		JButton btnNewButton = new JButton("검색");
+
+		btnNewButton = new JButton("검색");
 		btnNewButton.addActionListener(new ActionListener() {
+			private String findFoodName;
+
 			public void actionPerformed(ActionEvent arg0) {
-				// TODO 검색을 눌렀을 때 그 단어가 있는 음식들 찾기
-				
-				
-				
+				findFoodName = textField.getText();
+				System.out.println(findFoodName);
+				findFoodList(findFoodName);
+
 			}
 		});
-		springLayout.putConstraint(SpringLayout.WEST, btnNewButton, 225, SpringLayout.WEST, getContentPane());
-		springLayout.putConstraint(SpringLayout.EAST, textField, -6, SpringLayout.WEST, btnNewButton);
-		springLayout.putConstraint(SpringLayout.NORTH, btnNewButton, -1, SpringLayout.NORTH, textField);
+		springLayout.putConstraint(SpringLayout.NORTH, btnNewButton, 0, SpringLayout.NORTH, textField);
+		springLayout.putConstraint(SpringLayout.WEST, btnNewButton, 10, SpringLayout.EAST, textField);
 		getContentPane().add(btnNewButton);
-		
-		JMenuItem mntmNewMenuItem = new JMenuItem("안녕");
-		springLayout.putConstraint(SpringLayout.NORTH, mntmNewMenuItem, 327, SpringLayout.NORTH, getContentPane());
-		springLayout.putConstraint(SpringLayout.SOUTH, mntmNewMenuItem, -46, SpringLayout.SOUTH, getContentPane());
-		springLayout.putConstraint(SpringLayout.WEST, mntmNewMenuItem, 88, SpringLayout.WEST, getContentPane());
-		getContentPane().add(mntmNewMenuItem);
+
+		JLabel lblNewLabel = new JLabel("아침 :");
+		springLayout.putConstraint(SpringLayout.WEST, lblNewLabel, 80, SpringLayout.WEST, getContentPane());
+		springLayout.putConstraint(SpringLayout.SOUTH, lblNewLabel, -49, SpringLayout.SOUTH, getContentPane());
+		springLayout.putConstraint(SpringLayout.EAST, lblNewLabel, -351, SpringLayout.EAST, getContentPane());
+		getContentPane().add(lblNewLabel);
+
+		JLabel lblNewLabel_1 = new JLabel("");
+		springLayout.putConstraint(SpringLayout.NORTH, lblNewLabel_1, 0, SpringLayout.NORTH, lblNewLabel);
+		springLayout.putConstraint(SpringLayout.WEST, lblNewLabel_1, 6, SpringLayout.EAST, lblNewLabel);
+		springLayout.putConstraint(SpringLayout.SOUTH, lblNewLabel_1, -56, SpringLayout.SOUTH, getContentPane());
+		springLayout.putConstraint(SpringLayout.EAST, lblNewLabel_1, -131, SpringLayout.EAST, getContentPane());
+		getContentPane().add(lblNewLabel_1);
+
+		// 테이블 모델 초기화
+		tableModel = new DefaultTableModel();
+		tableModel.addColumn("음식명");
+		tableModel.addColumn("1회 제공량");
+		tableModel.addColumn("칼로리");
+
+		// JTable 초기화 및 모델 설정
+		resultTable = new JTable(tableModel);
+
+		// 더블클릭으로 셀 수정을 막기 위해 DefaultCellEditor를 사용하여 에디터 설정
+		resultTable.setDefaultEditor(Object.class, null);
+
+		// JTable에 ListSelectionListener 추가
+		resultTable.getSelectionModel().addListSelectionListener(new ListSelectionListener() {
+			@Override
+			public void valueChanged(ListSelectionEvent e) {
+				if (!e.getValueIsAdjusting()) {
+					int selectedRow = resultTable.getSelectedRow();
+					if (selectedRow >= 0) {
+						String foodName = resultTable.getValueAt(selectedRow, 0).toString();
+						String oneServing = resultTable.getValueAt(selectedRow, 1).toString();
+						String calories = resultTable.getValueAt(selectedRow, 2).toString();
+
+						System.out.println("선택된 항목: " + foodName + ", " + oneServing + ", " + calories);
+
+						// 선택된 행의 foodName을 lblNewLabel_1에 설정
+						lblNewLabel_1.setText(foodName);
+					}
+				}
+			}
+		});
+
+		// JScrollPane에 JTable 추가하여 스크롤 가능하도록 설정
+		JScrollPane scrollPane = new JScrollPane(resultTable);
+		springLayout.putConstraint(SpringLayout.NORTH, lblNewLabel, 187, SpringLayout.SOUTH, scrollPane);
+
+		// 컨테이너에 컴포넌트들 추가
+		springLayout.putConstraint(SpringLayout.NORTH, scrollPane, 10, SpringLayout.SOUTH, textField);
+		springLayout.putConstraint(SpringLayout.WEST, scrollPane, 10, SpringLayout.WEST, getContentPane());
+		springLayout.putConstraint(SpringLayout.SOUTH, scrollPane, 267, SpringLayout.SOUTH, textField);
+		getContentPane().add(scrollPane);
+
+		btnNewButton_1 = new JButton("확인");
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				// TODO 확인버튼을 누르면 디비 dietrecords테이블에 저장(DietRecords 클래스에서 디비(dietrecords테이블)로 저장할 수 있게 작성 후 여기서 호출하여 저장.)
+//				
+//				String selectQuery = "SELCT * FROM dietrecords WHERE user_id = ? AND date = ?";
+//				try (Connection conn = MySqlConnectionProvider.getConnection();
+//						PreparedStatement pst = conn.prepareStatement(selectQuery);) {
+//					pst.setString(1, user_id);
+//					pst.setDate(2, sqlDate);
+//					
+//					try (ResultSet rs = pst.executeQuery()) {
+//						if (!rs.next()) { // 그 데이터가 없는 경우. INSERT사용
+//							String insertQuery = "INSERT INTO dietrecords (user_id, date) VALUES (?,?)";
+//							try (PreparedStatement insertPst = conn.prepareStatement(insertQuery)) {
+//								insertPst.setString(1, user_id);
+//								insertPst.setDate(2, sqlDate);
+//
+//								// INSERT 실행
+//								int insertRow = insertPst.executeUpdate();
+//								if (insertRow == 1) {
+//									System.out.println("INSERT 성공");
+//								}
+//							}
+//
+//						} else { // 행이 있는 경우, UPDATE (아침만)
+//							
+//						}
+//					}
+//				} catch (SQLException e) {
+//					e.printStackTrace();
+//				}
+
+			}
+		});
+		springLayout.putConstraint(SpringLayout.NORTH, btnNewButton_1, -2, SpringLayout.NORTH, lblNewLabel);
+		springLayout.putConstraint(SpringLayout.WEST, btnNewButton_1, 5, SpringLayout.EAST, lblNewLabel_1);
+		getContentPane().add(btnNewButton_1);
 	}
-	
+
 	public static void main(String[] args) {
-		new FindFoodCalories();
+		new FindFoodCalories("asd");
 	}
+}
+
+class FoodData {
+	private String foodName;
+	private String oneServing;
+	private double calories;
+
+	public FoodData(String foodName, String oneServing, double calories) {
+		this.foodName = foodName;
+		this.oneServing = oneServing;
+		this.calories = calories;
+	}
+
+	public String getFoodName() {
+		return foodName;
+	}
+
+	public String getOneServing() {
+		return oneServing;
+	}
+
+	public double getCalories() {
+		return calories;
+	}
+
 }
