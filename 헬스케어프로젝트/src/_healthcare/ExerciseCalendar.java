35236fa407b2738cd9d02a63ceadb5fcbc6bcb75
Kanghyun2 <rkdguse@naver.com>
2024-02-23 15:56:55 +0900

    ㅠㅠ

--- 헬스케어프로젝트/src/_healthcare/ExerciseCalendar.java
+++ 헬스케어프로젝트/src/_healthcare/ExerciseCalendar.java
@@ -2,26 +2,24 @@ package _healthcare;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
 import java.time.LocalDate;
 import java.time.YearMonth;
 
-import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.SpringLayout;
 import javax.swing.SwingConstants;
 import javax.swing.SwingUtilities;
-import javax.swing.border.EmptyBorder;
-import javax.swing.SpringLayout;
+import javax.swing.border.LineBorder;
+import java.awt.Font;
 
 public class ExerciseCalendar extends JFrame {
 	private JLabel monthLabel; // 월을 표시하는 레이블
@@ -30,6 +28,8 @@ public class ExerciseCalendar extends JFrame {
 	private int currentYear; // 현재 연도를 나타내는 변수
 	protected int startDayOfWeek;
 	public String loginId;
+	private JPanel dayPanel;
+	private JLabel dayLabel;
 
 	public ExerciseCalendar(String loginId) {
 		this.loginId = loginId;
@@ -38,7 +38,7 @@ public class ExerciseCalendar extends JFrame {
 		setTitle("캘린다");
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setPreferredSize(new Dimension(360, 530));
-
+		
 		monthLabel = new JLabel("", SwingConstants.CENTER); // 월 표시 레이블 가운데 정렬
 		monthLabel.setBackground(Color.WHITE);
 		calendarPanel = new JPanel(new GridLayout(0, 7)); // 캘린더 패널에 그리드 레이아웃 설정
@@ -75,12 +75,12 @@ public class ExerciseCalendar extends JFrame {
 		JPanel controlsPanel = new JPanel();
 		controlsPanel.setBackground(Color.WHITE);
 		SpringLayout sl_controlsPanel = new SpringLayout();
-		sl_controlsPanel.putConstraint(SpringLayout.WEST, monthLabel, 142, SpringLayout.WEST, controlsPanel);
-		sl_controlsPanel.putConstraint(SpringLayout.EAST, monthLabel, -147, SpringLayout.EAST, controlsPanel);
-		sl_controlsPanel.putConstraint(SpringLayout.NORTH, nextButton, 0, SpringLayout.NORTH, prevButton);
-		sl_controlsPanel.putConstraint(SpringLayout.WEST, nextButton, 6, SpringLayout.EAST, monthLabel);
-		sl_controlsPanel.putConstraint(SpringLayout.NORTH, prevButton, 0, SpringLayout.NORTH, monthLabel);
-		sl_controlsPanel.putConstraint(SpringLayout.EAST, prevButton, -6, SpringLayout.WEST, monthLabel);
+		sl_controlsPanel.putConstraint(SpringLayout.EAST, prevButton, -220, SpringLayout.EAST, controlsPanel);
+		sl_controlsPanel.putConstraint(SpringLayout.WEST, monthLabel, 6, SpringLayout.EAST, prevButton);
+		sl_controlsPanel.putConstraint(SpringLayout.WEST, nextButton, 213, SpringLayout.WEST, controlsPanel);
+		sl_controlsPanel.putConstraint(SpringLayout.NORTH, prevButton, 0, SpringLayout.NORTH, controlsPanel);
+		sl_controlsPanel.putConstraint(SpringLayout.EAST, monthLabel, -6, SpringLayout.WEST, nextButton);
+		sl_controlsPanel.putConstraint(SpringLayout.NORTH, nextButton, 0, SpringLayout.NORTH, controlsPanel);
 		sl_controlsPanel.putConstraint(SpringLayout.NORTH, monthLabel, 0, SpringLayout.NORTH, controlsPanel);
 		sl_controlsPanel.putConstraint(SpringLayout.SOUTH, monthLabel, -7, SpringLayout.SOUTH, controlsPanel);
 		controlsPanel.setLayout(sl_controlsPanel);
@@ -116,6 +116,56 @@ public class ExerciseCalendar extends JFrame {
 		sl_controlsPanel.putConstraint(SpringLayout.EAST, btnBack, 31, SpringLayout.WEST, controlsPanel);
 		controlsPanel.add(btnBack);
 		getContentPane().add(calendarPanel);
+		
+		JLabel lblNewLabel = new JLabel("월");
+		springLayout.putConstraint(SpringLayout.WEST, lblNewLabel, 20, SpringLayout.WEST, getContentPane());
+		springLayout.putConstraint(SpringLayout.SOUTH, lblNewLabel, -17, SpringLayout.NORTH, calendarPanel);
+		springLayout.putConstraint(SpringLayout.EAST, lblNewLabel, -305, SpringLayout.EAST, getContentPane());
+		lblNewLabel.setForeground(Color.LIGHT_GRAY);
+		lblNewLabel.setFont(new Font("한컴산뜻돋움", Font.PLAIN, 14));
+		getContentPane().add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("화\r\n");
+		springLayout.putConstraint(SpringLayout.NORTH, lblNewLabel_1, 0, SpringLayout.NORTH, lblNewLabel);
+		springLayout.putConstraint(SpringLayout.WEST, lblNewLabel_1, 25, SpringLayout.EAST, lblNewLabel);
+		lblNewLabel_1.setForeground(Color.LIGHT_GRAY);
+		lblNewLabel_1.setFont(new Font("한컴산뜻돋움", Font.PLAIN, 14));
+		getContentPane().add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("수");
+		springLayout.putConstraint(SpringLayout.SOUTH, lblNewLabel_2, -17, SpringLayout.NORTH, calendarPanel);
+		springLayout.putConstraint(SpringLayout.EAST, lblNewLabel_2, -219, SpringLayout.EAST, getContentPane());
+		lblNewLabel_2.setForeground(Color.LIGHT_GRAY);
+		lblNewLabel_2.setFont(new Font("한컴산뜻돋움", Font.PLAIN, 14));
+		getContentPane().add(lblNewLabel_2);
+		
+		JLabel lblNewLabel_3 = new JLabel("목");
+		springLayout.putConstraint(SpringLayout.NORTH, lblNewLabel_3, 0, SpringLayout.NORTH, lblNewLabel);
+		springLayout.putConstraint(SpringLayout.WEST, lblNewLabel_3, 37, SpringLayout.EAST, lblNewLabel_2);
+		lblNewLabel_3.setForeground(Color.LIGHT_GRAY);
+		lblNewLabel_3.setFont(new Font("한컴산뜻돋움", Font.PLAIN, 14));
+		getContentPane().add(lblNewLabel_3);
+		
+		JLabel lblNewLabel_4 = new JLabel("금");
+		springLayout.putConstraint(SpringLayout.NORTH, lblNewLabel_4, 0, SpringLayout.NORTH, lblNewLabel);
+		lblNewLabel_4.setForeground(Color.LIGHT_GRAY);
+		lblNewLabel_4.setFont(new Font("한컴산뜻돋움", Font.PLAIN, 14));
+		getContentPane().add(lblNewLabel_4);
+		
+		JLabel lblNewLabel_5 = new JLabel("토");
+		springLayout.putConstraint(SpringLayout.EAST, lblNewLabel_4, -34, SpringLayout.WEST, lblNewLabel_5);
+		springLayout.putConstraint(SpringLayout.NORTH, lblNewLabel_5, 0, SpringLayout.NORTH, lblNewLabel);
+		lblNewLabel_5.setForeground(Color.LIGHT_GRAY);
+		lblNewLabel_5.setFont(new Font("한컴산뜻돋움", Font.PLAIN, 14));
+		getContentPane().add(lblNewLabel_5);
+		
+		JLabel lblNewLabel_6 = new JLabel("일");
+		springLayout.putConstraint(SpringLayout.EAST, lblNewLabel_5, -36, SpringLayout.WEST, lblNewLabel_6);
+		springLayout.putConstraint(SpringLayout.NORTH, lblNewLabel_6, 18, SpringLayout.SOUTH, controlsPanel);
+		springLayout.putConstraint(SpringLayout.EAST, lblNewLabel_6, -21, SpringLayout.EAST, getContentPane());
+		lblNewLabel_6.setForeground(Color.LIGHT_GRAY);
+		lblNewLabel_6.setFont(new Font("한컴산뜻돋움", Font.PLAIN, 14));
+		getContentPane().add(lblNewLabel_6);
 
 		pack(); // 컴포넌트들을 적절한 크기로 정렬
 		setLocationRelativeTo(null); // 화면 중앙에 표시
@@ -125,40 +175,40 @@ public class ExerciseCalendar extends JFrame {
 		YearMonth yearMonth = YearMonth.of(currentYear, currentMonth + 1);//  현재 연도와 월로 YearMonth 객체 생성
 		switch (yearMonth.getMonth()) {
 		case JANUARY:
-			monthLabel.setText("1월" + " " + yearMonth.getYear() + "년");
+			monthLabel.setText(yearMonth.getYear() + "년" + "1월");
 			break;
 		case FEBRUARY:
-			monthLabel.setText("2월" + " " + yearMonth.getYear() + "년");
+			monthLabel.setText(yearMonth.getYear() + "년" + "2월");
 			break;
 		case MARCH:
-			monthLabel.setText("3월" + " " + yearMonth.getYear() + "년");
+			monthLabel.setText(yearMonth.getYear() + "년" + "3월");
 			break;
 		case APRIL:
-			monthLabel.setText("4월" + " " + yearMonth.getYear() + "년");
+			monthLabel.setText(yearMonth.getYear() + "년" + "4월");
 			break;
 		case MAY:
-			monthLabel.setText("5월" + " " + yearMonth.getYear() + "년");
+			monthLabel.setText(yearMonth.getYear() + "년" + "5월");
 			break;
 		case JUNE:
-			monthLabel.setText("6월" + " " + yearMonth.getYear() + "년");
+			monthLabel.setText(yearMonth.getYear() + "년" + "6월");
 			break;
 		case JULY:
-			monthLabel.setText("7월" + " " + yearMonth.getYear() + "년");
+			monthLabel.setText(yearMonth.getYear() + "년" + "7월");
 			break;
 		case AUGUST:
-			monthLabel.setText("8월" + " " + yearMonth.getYear() + "년");
+			monthLabel.setText(yearMonth.getYear() + "년" + "8월");
 			break;
 		case SEPTEMBER:
-			monthLabel.setText("9월" + " " + yearMonth.getYear() + "년");
+			monthLabel.setText(yearMonth.getYear() + "년" + "9월");
 			break;
 		case OCTOBER:
-			monthLabel.setText("10월" + " " + yearMonth.getYear() + "년");
+			monthLabel.setText(yearMonth.getYear() + "년" + "10월");
 			break;
 		case NOVEMBER:
-			monthLabel.setText("11월" + " " + yearMonth.getYear() + "년");
+			monthLabel.setText(yearMonth.getYear() + "년" + "11월");
 			break;
 		case DECEMBER:
-			monthLabel.setText("12월" + " " + yearMonth.getYear() + "년");
+			monthLabel.setText(yearMonth.getYear() + "년" + "12월");
 			break;
 		default:
 			break;
@@ -171,62 +221,79 @@ public class ExerciseCalendar extends JFrame {
 		for (int i = 1; i < startDayOfWeek; i++) { // 첫째 날이 시작되는 요일 전까지 공백 레이블 추가
 			calendarPanel.add(new JLabel(""));
 		}
-
+		
+		
 		for (int day = 1; day <= daysInMonth; day++) { // 해당 월의 날짜만큼 패널에 추가
-			JPanel dayPanel = new JPanel(new BorderLayout());
-			dayPanel.setBackground(Color.WHITE);
+		    dayPanel = new JPanel(new BorderLayout());
+		    dayPanel.setBackground(Color.WHITE);
+//		    dayPanel.setBorder(new LineBorder(Color.BLACK));
+		    
+		    // 텍스트 레이블 추가
+		    dayLabel = new JLabel("", SwingConstants.CENTER);
+		    dayLabel.setBackground(Color.WHITE); // 배경색을 하얀색으로 설정
+		    dayLabel.setOpaque(true); // 불투명하게 설정
+		    dayLabel.setPreferredSize(new Dimension(50, 50));
+		    dayPanel.add(dayLabel, BorderLayout.CENTER); // 텍스트 레이블을 패널에 추가
+		    
+		    // 이미지 레이블 추가
+		    String image = "Date" + day + ".png";
+		    ImageIcon imageIcon = new ImageIcon(image); // 이미지 경로 설정
+		    JLabel imageLabel = new JLabel(imageIcon);
+		    dayPanel.add(imageLabel, BorderLayout.NORTH); // 이미지 레이블을 패널에 추가
+		    
+		    // --- 라벨 추가
+		    JLabel kcalLabel = new JLabel("---", SwingConstants.CENTER);
+		    dayPanel.add(kcalLabel, BorderLayout.CENTER); // --- 라벨을 패널에 추가
 
-			// 이미지를 추가할 경우
-			try {
-				String imageName = "Defaultimage" + day + ".png";// 이미지 파일 경로 설정
-				File imageFile = new File(imageName);
-				BufferedImage image = ImageIO.read(imageFile); // 이미지 파일 읽기
-				JLabel imageLabel = new JLabel(new ImageIcon(image), SwingConstants.CENTER); // 이미지를 표시할 레이블 생성
-				imageLabel.setBackground(Color.WHITE); // 배경색을 하얀색으로 설정
-				imageLabel.setOpaque(true); // 불투명하게 설정
-				imageLabel.setPreferredSize(new Dimension(50, 50));
-				dayPanel.add(imageLabel, BorderLayout.CENTER); // 이미지 레이블을 패널에 추가
+		    if (day == 15) {
+		        String newImage = "Check15.png"; // 새로운 이미지 경로
+		        ImageIcon newImageIcon = new ImageIcon(newImage); // 새로운 이미지 아이콘 생성
+		        imageLabel.setIcon(newImageIcon); // 이미지 레이블에 새로운 이미지 설정
+		    }
 
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-
-			calendarPanel.add(dayPanel); // 날짜 패널을 캘린더 패널에 추가
-		}
-
-		revalidate(); // 컴포넌트들을 다시 배치하고 그리기 위해 호출
-		repaint(); // 컴포넌트를 다시 그리도록 요청
-	}
-
-	public void updateCalendarImage(int dayOfMonth) {
-		try {
-			String imageName = "Date" + dayOfMonth + ".png"; // 이미지 파일 경로 설정
-			File imageFile = new File(imageName);
-			BufferedImage image = ImageIO.read(imageFile); // 이미지 파일 읽기
-//			System.out.println(imageName);
-//			System.out.println(image);
-			// 해당 날짜의 패널을 찾아 이미지를 변경
-			JPanel dayPanel = (JPanel) calendarPanel.getComponent((dayOfMonth - 1) + startDayOfWeek - 1); // dayOfMonth를
-																											// 0부터 시작하도록
-																											// 조정
-			JLabel imageLabel = (JLabel) dayPanel.getComponent(0); // 이미지가 있는 첫 번째 컴포넌트 가져오기
-			imageLabel.setIcon(new ImageIcon(image)); // 이미지 변경
-//			imageLabel.setBackground(Color.WHITE); // 배경색을 하얀색으로 설정
-//			imageLabel.setOpaque(true); // 불투명하게 설정
-//			imageLabel.setPreferredSize(new Dimension(50, 50));
-			dayPanel.add(imageLabel, BorderLayout.CENTER); // 이미지 레이블을 패널에 추가
-//			System.out.println(dayPanel);
-//			System.out.println(imageLabel);
-			revalidate(); // 컴포넌트들을 다시 배치하고 그리기 위해 호출
-			repaint(); // 컴포넌트를 다시 그리도록 요청
-			System.out.println(calendarPanel.getComponent((dayOfMonth - 1) + startDayOfWeek - 1)); // 확인을 위한 출력
-		} catch (IOException e) {
-			e.printStackTrace();
+		    
+		    calendarPanel.add(dayPanel); // 날짜 패널을 캘린더 패널에 추가
 		}
 	}
+	
+	
+//	public void displayExerciseImage(byte[] imageBytes) {
+//	    // 바이트 배열을 ImageIcon으로 변환
+//	    ImageIcon imageIcon = new ImageIcon(imageBytes);
+//
+//	    // 현재 날짜를 가져옴
+//	    LocalDate currentDate = LocalDate.now();
+//	    int currentDay = currentDate.getDayOfMonth();
+//
+//	    // 캘린더 패널에서 해당 날짜의 패널을 찾아 이미지를 추가
+//	    for (Component component : calendarPanel.getComponents()) {
+//	        if (component instanceof JPanel) {
+//	             dayPanel = (JPanel) component;
+//	            for (Component subComponent : dayPanel.getComponents()) {
+//	                if (subComponent instanceof JLabel) {
+//	                     dayLabel = (JLabel) subComponent;
+//	                    String labelText = dayLabel.getText();
+//	                    if (labelText.equals(") {
+//	                        // 이미지 아이콘을 라벨에 설정
+//	                        dayLabel.setIcon(imageIcon);
+//	                        break; // 이미지를 추가했으므로 반복문 종료
+//	                    }
+//	                }
+//	            }
+//	        }
+//	    }
+//
+//	    // 캘린더를 다시 그리도록 요청
+//	    revalidate();
+//	    repaint();
+//	}
+
 
 	public static void main(String[] args) {
 		SwingUtilities.invokeLater(() -> {
 		});
+		// SwingUtilities.invokeLater()는 스윙(Swing) GUI 업데이트 작업을 안전하게 처리하기 위한 방법 중 하나입니다. Swing은 단일 스레드 모델(single-threaded model)을 사용하기 때문에, GUI 컴포넌트에 대한 작업은 주로 이벤트 디스패치 스레드(Event Dispatch Thread)에서 처리됩니다.
+// 따라서 다른 스레드에서 GUI를 변경해야 하는 경우, SwingUtilities.invokeLater()를 사용하여 해당 작업을 이벤트 디스패치 스레드로 보내어 안전하게 처리할 수 있습니다. 이것은 다른 스레드에서 GUI 업데이트를 수행할 때 발생할 수 있는 동시성 문제를 방지하는데 도움이 됩니다.
+// 즉, 위의 코드는 빈 람다식을 invokeLater()에 전달하고 있으므로, 이 안에 GUI 관련 작업을 넣으면 됩니다. 이렇게 하면 해당 작업이 이벤트 디스패치 스레드에서 실행됩니다.
 	}
 }
