36df67af3f9b7d43568e3f5834f8766ecd520b5c
gayoung <7rmile@gmail.com>
2024-02-23 18:08:39 +0900

    물

--- 헬스케어프로젝트/src/_healthcare/WaterRecords.java
+++ 헬스케어프로젝트/src/_healthcare/WaterRecords.java
@@ -20,26 +20,28 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import javax.swing.ImageIcon;
+import java.awt.Color;
 
 public class WaterRecords extends JFrame {
 	int cup = 0;
 	private JLabel nowWaterDrinking;
-	private JToggleButton btn_cup1;
-	private JToggleButton btn_cup2;
-	private JToggleButton btn_cup3;
-	private JToggleButton btn_cup4;
-	private JToggleButton btn_cup5;
-	private JToggleButton btn_cup6;
-	private JToggleButton btn_cup7;
-	private JToggleButton btn_cup8;
 	// 현재 날짜
 	java.util.Date currentDate = new java.util.Date();
 	// sql에 넣기 위해 날짜를 date형식으로 변경
 	Date sqlDate = new Date(currentDate.getTime());
 
 	private String user_id;
-	
-	public WaterRecords(String loginId) { // 오늘 데이트와 아이디로 select해보고/ 행이 없다면 행 insert./ 행이 있다면 행 update.
+	private JButton btn_cup1;
+	private JButton btn_cup2;
+	private JButton btn_cup3;
+	private JButton btn_cup4;
+	private JButton btn_cup5;
+	private JButton btn_cup6;
+	private JButton btn_cup7;
+	private JButton btn_cup8;
+
+	public WaterRecords(String loginId) {
+		getContentPane().setBackground(Color.WHITE); // 오늘 데이트와 아이디로 select해보고/ 행이 없다면 행 insert./ 행이 있다면 행 update.
 		this.user_id = loginId;
 		try (Connection conn = MySqlConnectionProvider.getConnection()) {
 			String selectQuery = "SELECT * FROM waterrecords WHERE user_id = ? AND date = ?";
@@ -68,14 +70,12 @@ public class WaterRecords extends JFrame {
 				}
 
 			}
-			
+
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		
 
 		extracted();
-		clickCup(); // 컵을 클릭했을 때
 		showGUI();
 
 	}
@@ -86,11 +86,9 @@ public class WaterRecords extends JFrame {
 		setVisible(true);
 	}
 
-	
-
 	private void updateCupCount() {
 		nowWaterDrinking.setText(String.valueOf(cup));
-		updateDatabase(); 
+		updateDatabase();
 	}
 
 	// 디비를 업데이트하는 메소드
@@ -118,65 +116,22 @@ public class WaterRecords extends JFrame {
 
 	}
 
-	private void clickCup() {
-		// System.out.println("Debug: Entering clickCup method"); // 디버깅용 출력 추가
-		JToggleButton[] cupBtns = { btn_cup1, btn_cup2, btn_cup3, btn_cup4, btn_cup5, btn_cup6, btn_cup7, btn_cup8 };
-		
-		for (JToggleButton cupBtn : cupBtns) {
-		    if (cupBtn != null) { // null 체크 추가
-		        cupBtn.addItemListener(new ItemListener() {
-		            @Override
-		            public void itemStateChanged(ItemEvent e) {
-		                // System.out.println("Debug: Inside itemStateChanged"); // 디버깅용 출력 추가
-		                if (e.getStateChange() == ItemEvent.SELECTED) {
-		                    cup++;
-		                    
-		                } else {
-		                    cup--;
-		                }
-		                
-		                updateCupCount();
-		            }
-		        });
-		    }
-		}
 
-	}
-	
 	private void extracted() {
 		setTitle("물 기록");
 		getContentPane().setLayout(null);
 
-		btn_cup7 = new JToggleButton("물 한 잔");
-		btn_cup7.setBounds(211, 130, 77, 23);
-		getContentPane().add(btn_cup7);
-
-		btn_cup1 = new JToggleButton("");
-		btn_cup1.setIcon(new ImageIcon(WaterRecords.class.getResource("/image/물마시기전 1.png")));
-		btn_cup1.setBounds(44, 101, 74, 68);
-		btn_cup1.setContentAreaFilled(false);
-		btn_cup1.setBorderPainted(false);
-		btn_cup1.setFocusPainted(false);
-		getContentPane().add(btn_cup1);
-
-		btn_cup2 = new JToggleButton("물 한 잔");
-		btn_cup2.setBounds(130, 101, 77, 23);
+		btn_cup2 = new JButton("");
+		btn_cup2.setIcon(new ImageIcon(WaterRecords.class.getResource("/image/물마시기전 2.png")));
+		btn_cup2.setFocusPainted(false);
+		btn_cup2.setContentAreaFilled(false);
+		btn_cup2.setBorderPainted(false);
+		btn_cup2.setBounds(132, 93, 52, 65);
+		btnClick(btn_cup2);
 		getContentPane().add(btn_cup2);
 
-		btn_cup3 = new JToggleButton("물 한 잔");
-		btn_cup3.setBounds(211, 101, 77, 23);
-		getContentPane().add(btn_cup3);
-
-		btn_cup5 = new JToggleButton("물 한 잔");
-		btn_cup5.setBounds(44, 195, 77, 23);
-		getContentPane().add(btn_cup5);
-
-		btn_cup6 = new JToggleButton("물 한 잔");
-		btn_cup6.setBounds(130, 130, 77, 23);
-		getContentPane().add(btn_cup6);
-
 		JLabel lblNewLabel = new JLabel("물 권장량");
-		lblNewLabel.setBounds(48, 57, 52, 15);
+		lblNewLabel.setBounds(12, 69, 52, 15);
 		getContentPane().add(lblNewLabel);
 
 		JLabel lblNewLabel_1 = new JLabel("물 음용량: ");
@@ -184,11 +139,11 @@ public class WaterRecords extends JFrame {
 		getContentPane().add(lblNewLabel_1);
 
 		nowWaterDrinking = new JLabel("0");
-		nowWaterDrinking.setBounds(132, 250, 6, 15);
+		nowWaterDrinking.setBounds(144, 250, 40, 15);
 		getContentPane().add(nowWaterDrinking);
 
 		JLabel lblNewLabel_3 = new JLabel(" / 8잔");
-		lblNewLabel_3.setBounds(162, 250, 32, 15);
+		lblNewLabel_3.setBounds(202, 250, 46, 15);
 		getContentPane().add(lblNewLabel_3);
 
 		JLabel lblNewLabel_4 = new JLabel("<html> 세계보건기구(WHO)가 권장하는 하루 물 섭취량은 1.5~2ℓ입니다. <br/r>"
@@ -197,14 +152,87 @@ public class WaterRecords extends JFrame {
 		lblNewLabel_4.setFont(new Font("돋움", Font.PLAIN, 10));
 		getContentPane().add(lblNewLabel_4);
 
-		btn_cup4 = new JToggleButton("물 한 잔");
-		btn_cup4.setBounds(294, 101, 77, 23);
+		btn_cup1 = new JButton("");
+		btn_cup1.setIcon(new ImageIcon(WaterRecords.class.getResource("/image/물마시기전 2.png")));
+		btn_cup1.setBounds(64, 93, 52, 65);
+		btn_cup1.setContentAreaFilled(false);
+		btn_cup1.setBorderPainted(false);
+		btn_cup1.setFocusPainted(false);
+		btnClick(btn_cup1);
+		getContentPane().add(btn_cup1);
+
+		btn_cup3 = new JButton("");
+		btn_cup3.setIcon(new ImageIcon(WaterRecords.class.getResource("/image/물마시기전 2.png")));
+		btn_cup3.setFocusPainted(false);
+		btn_cup3.setContentAreaFilled(false);
+		btn_cup3.setBorderPainted(false);
+		btn_cup3.setBounds(196, 93, 52, 65);
+		btnClick(btn_cup3);
+		getContentPane().add(btn_cup3);
+
+		btn_cup4 = new JButton("");
+		btn_cup4.setIcon(new ImageIcon(WaterRecords.class.getResource("/image/물마시기전 2.png")));
+		btn_cup4.setFocusPainted(false);
+		btn_cup4.setContentAreaFilled(false);
+		btn_cup4.setBorderPainted(false);
+		btn_cup4.setBounds(260, 93, 52, 65);
+		btnClick(btn_cup4);
 		getContentPane().add(btn_cup4);
 
-		btn_cup8 = new JToggleButton("물 한 잔");
-		btn_cup8.setBounds(294, 130, 77, 23);
+		btn_cup5 = new JButton("");
+		btn_cup5.setIcon(new ImageIcon(WaterRecords.class.getResource("/image/물마시기전 2.png")));
+		btn_cup5.setFocusPainted(false);
+		btn_cup5.setContentAreaFilled(false);
+		btn_cup5.setBorderPainted(false);
+		btn_cup5.setBounds(64, 168, 52, 65);
+		btnClick(btn_cup5);
+		getContentPane().add(btn_cup5);
+
+		btn_cup6 = new JButton("");
+		btn_cup6.setIcon(new ImageIcon(WaterRecords.class.getResource("/image/물마시기전 2.png")));
+		btn_cup6.setFocusPainted(false);
+		btn_cup6.setContentAreaFilled(false);
+		btn_cup6.setBorderPainted(false);
+		btn_cup6.setBounds(132, 168, 52, 65);
+		btnClick(btn_cup6);
+		getContentPane().add(btn_cup6);
+
+		btn_cup7 = new JButton("");
+		btn_cup7.setIcon(new ImageIcon(WaterRecords.class.getResource("/image/물마시기전 2.png")));
+		btn_cup7.setFocusPainted(false);
+		btn_cup7.setContentAreaFilled(false);
+		btn_cup7.setBorderPainted(false);
+		btn_cup7.setBounds(196, 168, 52, 65);
+		btnClick(btn_cup7);
+		getContentPane().add(btn_cup7);
+
+		btn_cup8 = new JButton("");
+		btn_cup8.setIcon(new ImageIcon(WaterRecords.class.getResource("/image/물마시기전 2.png")));
+		btn_cup8.setFocusPainted(false);
+		btn_cup8.setContentAreaFilled(false);
+		btn_cup8.setBorderPainted(false);
+		btn_cup8.setBounds(260, 168, 52, 65);
+		btnClick(btn_cup8);
 		getContentPane().add(btn_cup8);
 	}
-	
-	
+
+	private void btnClick(JButton btn) {
+		btn.addActionListener(new ActionListener() {
+			private boolean clicked = false;
+
+			public void actionPerformed(ActionEvent e) {
+				if (clicked) {
+					btn.setIcon(new ImageIcon(WaterRecords.class.getResource("/image/물마시기전 2.png")));
+					cup--;
+				} else {
+					btn.setIcon(new ImageIcon(WaterRecords.class.getResource("/image/물마신후 2.png")));
+					cup++;
+				}
+				updateCupCount();
+				clicked = !clicked; // 클릭 상태를 토글
+			}
+		});
+
+	}
+
 }
